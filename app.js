fde4e4c89284c4d713eb31e5a7b2a35d22640eed
shubham3751 <58082648+shubham3751@users.noreply.github.com>
2021-04-26 18:01:14 +0530

    added procfile and update ports

--- app.js
+++ app.js
@@ -222,7 +222,12 @@ app.post("/login", function(req, res){
       //  }
     //});
 
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+      port = 3000;
+    }
+    app.listen(port);
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Server started on port 3000.");
 });
