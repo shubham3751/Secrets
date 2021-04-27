5a099db9b1a9b561b345d3b494f2b0869865ab77
shubham3751 <58082648+shubham3751@users.noreply.github.com>
2021-04-27 18:18:26 +0530

    second commit

--- app.js
+++ app.js
@@ -222,12 +222,12 @@ app.post("/login", function(req, res){
       //  }
     //});
 
-    let port = process.env.PORT;
-    if (port == null || port == "") {
-      port = 3000;
-    }
-    app.listen(port);
+    // let port = process.env.PORT;
+    // if (port == null || port == "") {
+    //   port = 3000;
+    // }
+    // app.listen(port);
 
-app.listen(port, function(){
+app.listen(process.env.PORT || 3000 , function(){
   console.log("Server started on port 3000.");
 });
